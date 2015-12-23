f1ababc15354d34e6548098cf7b33f4ac153d478
dogsAreReallyCool <zachary.witzofsky@bsd7students.org>
2015-12-23 04:32:44 +0000

    Fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
-  get '', to: 'static_pages#index'
-  get 'login', to: 'static_pages#login'
+    get 'sessions/new'
+
+    get 'users/new'
+
+    get '', to: 'static_pages#index'
+    get 'dashboard', to: 'static_pages#dashboard'
+  
+    get "log_out" => "sessions#destroy", :as => "log_out"
+    get "log_in" => "sessions#new", :as => "log_in"
+    get "sign_up" => "users#new", :as => "sign_up"
+    root :to => "users#new"
+    resources :users
+    resources :sessions
 end
